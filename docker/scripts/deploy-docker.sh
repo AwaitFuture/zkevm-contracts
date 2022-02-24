86f9db29fb791627bd264fa3498c301cb1aa186c
invocamanman <jesus.ligero@yahoo.es>
2022-02-24 13:31:17 +0100

    update types poe

--- docker/scripts/deploy-docker.sh
+++ docker/scripts/deploy-docker.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
+set -x
 sudo rm -rf docker/gethData/geth_data
+sudo mkdir docker/gethData/geth_data
 DEV_PERIOD=0 docker-compose -f docker/docker-compose.geth.yml up -d geth
 sleep 5
 node docker/scripts/fund-accounts.js
@@ -7,4 +9,4 @@ npx hardhat run deployment/testnet/deployPoETestnet.js --network localhost
 mkdir docker/deploymentOutput
 mv deployment/testnet/deploy_output.json docker/deploymentOutput
 docker-compose -f docker/docker-compose.geth.yml down
-sudo docker build -t hermeznetwork/geth-zkevm-contracts -f docker/Dockerfile.geth .
+sudo docker build -t geth-zkevm-contracts -f docker/Dockerfile.geth .
